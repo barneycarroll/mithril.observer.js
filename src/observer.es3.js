f21b2b1d1a15e68325db7f2ae7e257a0ae7394d7
Barney Carroll <barneycarroll@users.noreply.github.com>
2016-02-15 16:17:25 +0000

    Create observer.es3.js

---
+++ src/observer.es3.js
@@ -0,0 +1,41 @@
+( function(){
+  var observers = []
+
+  var config    = {
+    childList             : true,
+    attributes            : true,
+    characterData         : true,
+    subtree               : true,
+    attributeOldValue     : true,
+    characterDataOldValue : true,
+    attributeFilte        : true
+  }
+
+  m.mount( document.createElement( 'div' ), {
+    view :function(){
+      return m( 'div', {
+        config : function(){
+          for( var i = 0; i < observers.length; i++ )
+            observers[ i ].disconnect()
+          
+          observers.length = 0
+        }
+      } )
+    }
+  } )
+
+  var m_render = m.render
+
+  m.render = function( root ){
+    var observer = new MutationObserver( function( mutation ){
+      for( var i = 0; i < mutations.length; i++ )
+        console.log( mutations[ i ] )
+    } )
+    
+    observer.observe( root, config )
+  
+    observers.push( observer )
+    
+    return m_render.apply( this, arguments )
+  }
+}() );
